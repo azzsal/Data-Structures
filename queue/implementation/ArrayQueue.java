fb8d0bf32429c07cb5edfc5719cbeaf274b2ad91
azzsal <azzsal@protonmail.com>
2023-04-12 22:36:46 +0200

    Minor fix

--- queue/implementation/ArrayQueue.java
+++ queue/implementation/ArrayQueue.java
@@ -2,7 +2,7 @@ package queue.implementation;
 
 /**
  * The Queue represents a first-in-first-out queue of generic items.
- * It supports enqueue, dequeue and nextFront operations.
+ * It supports enqueue, dequeue and front operations.
  * this implementation is based on circular arrays.
  * @param <T>
  * @author aziz
@@ -11,12 +11,12 @@ public class ArrayQueue<T> implements Queue<T> {
 
     private T[] items;
     private int size;
-    private int nextFront;
+    private int front;
     private int nextBack;
 
     public ArrayQueue() {
         items = (T[]) new Object[8];
-        nextFront = 0;
+        front = 0;
         nextBack = 0;
         size = 0;
     }
@@ -38,29 +38,29 @@ public class ArrayQueue<T> implements Queue<T> {
 
     private void resize(int capacity) {
         T[] temp = (T[]) new Object[capacity];
-        int startIndex = nextFront;
+        int startIndex = front;
         for(int i = 0; i < size; i++) {
             temp[i] = items[startIndex];
             startIndex = (startIndex + 1) % items.length;
         }
         items = temp;
-        nextFront = 0;
+        front = 0;
         nextBack = size;
     }
 
     /**
-     * Removes and returns the item at the nextFront of
+     * Removes and returns the item at the front of
      * the queue if present, else null.<br/>
      * Time complexity: amortized constant time &Theta;(<em>1</em>)
-     * @return the item at nextFront of the queue
+     * @return the item at front of the queue
      */
     @Override
     public T dequeue() {
         if(isEmpty()) {
             return null;
         }
-        T item = items[nextFront];
-        nextFront = (nextFront + 1) % items.length;
+        T item = items[front];
+        front = (front + 1) % items.length;
         size -= 1;
         if (items.length >= 16 && getUsageRatio() < 0.25) {
             resize(items.length / 2);
@@ -73,17 +73,17 @@ public class ArrayQueue<T> implements Queue<T> {
     }
 
     /**
-     * Returns the item at the nextFront of the queue
+     * Returns the item at the front of the queue
      * if present, else null.<br/>
      * Time complexity: constant time &Theta;(<em>1</em>)
-     * @return the item at nextFront of the queue
+     * @return the item at front of the queue
      */
     @Override
     public T front() {
         if(isEmpty()) {
             return null;
         }
-        return items[nextFront];
+        return items[front];
     }
 
     /**
